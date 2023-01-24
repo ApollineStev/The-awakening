983c9955e998af12e8418be3fb00540969e83729
ApollineStev <apollinecanard@gmail.com>
2023-01-24 18:32:30 +0100

    game over added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let start = document.querySelector('#start')
+let replay = document.querySelector('#replay')
 
 let pentacle = document.querySelectorAll('.pentacle')
 let ghost = document.querySelectorAll('.ghost')
@@ -15,40 +16,80 @@ let score = 0
 
 start.addEventListener('click', () => {
     start.remove('id')
+    cleanBoard()
     moveGhost()
-    timeOut
-    gameOver()
+
 })
+
+
 
 function randomPentacle() {
     
     ghost.forEach(element => {
-        element.classList.remove('ghost')
-           
+        element.classList.remove('ghost')       
     });
-    let randomPentacle = ghost[Math.floor(Math.random() * 12)]
+
+    let randomPentacle = ghost[Math.floor(Math.random() * ghost.length)]
     randomPentacle.classList.add('ghost')
+
+    
     setTimeout(() => {
         if (randomPentacle.classList == 'ghost'){
-            
             randomPentacle.classList.add('worm')
-            randomPentacle.classList.add('ghost')
-        
+            wormSum += 1
+            life.innerHTML = wormSum
+            return randomPentacle
         }
+        return wormSum
         
     }, 1000)
 
-    /*ghost.forEach(element => {
-        element.classList.contains('worm')
-        wormSum += 1
-        life.innerHTML = wormSum
+
+    ghost.forEach(ghost => {
+        if(ghost.classList == 'worm'){
+            return randomPentacle
+        }
     })
-    */
+
+    
+
+    if(wormSum === 5){
+        end.style.visibility = "visible"
+        console.log('game over')
+    }
+
+    
+
 }
 
 function moveGhost() {
    let timer = null
-   timer = setInterval(randomPentacle, 1001)
+   timer = setInterval(randomPentacle, 1250)
+   if(ghost.classList == 'worm'){
+    let randomPentacle = ghost[Math.floor(Math.random() * ghost.length)]
+    randomPentacle.classList.add('ghost')
+    return timer
+   }
+}
+
+replay.addEventListener('click', () => {
+    end.style.visibility = 'hidden'
+    cleanBoard()
+    moveGhost()
+
+    
+})
+
+function cleanBoard() {
+    score = 0
+    result.innerHTML = score
+    wormSum = 0
+    life.innerHTML = wormSum
+    ghost.forEach(ghost => {
+        ghost.classList.remove('ghost')
+        ghost.classList.remove('worm')
+    })
+    
 }
 
 ghost.forEach(ghost => {
@@ -69,13 +110,4 @@ ghost.forEach(ghost => {
 
 
 
-
-function gameOver() {
-   if(score >= 30){
-    end.style.visibility = "visible"
-    score = 0
-    wormSum = 0
-
-   }
-}
 
